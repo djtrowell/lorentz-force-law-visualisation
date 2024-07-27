0ccac855035558fc2cbc6a189396280c28917b05
djtrowell <contact.djtrowell@gmail.com>
2024-07-27 17:16:55 +0100

    Update vector field functions

--- main.m
+++ main.m
@@ -3,25 +3,25 @@ function main ()
   clf;
   f = figure (1);
   
-  min = -10; 
-  max = 10; 
+  min = -25; 
+  max = 25; 
   [x y] = meshgrid (min:0.5:max);
   
   % Define electric vector field
-  x_E = @(x,y) 0*(x+0.001)^2+0.5;
-  y_E = @(x,y) (y+0.005)^2+0.5;
+  x_E = @(x, y) -(sqrt(x.^2 + y.^2) - 10) .* (x ./ sqrt(x.^2 + y.^2)) - (y ./ sqrt(x.^2 + y.^2));
+  y_E = @(x, y) -(sqrt(x.^2 + y.^2) - 10) .* (y ./ sqrt(x.^2 + y.^2)) + (x ./ sqrt(x.^2 + y.^2));
   E = quiver (x, y, x_E(x,y), y_E(x, y), color='b'); hold on
   % Define magnetic vector field
-  x_B = @(x,y) 0*x;
-  y_B = @(x,y) 0*y;
+  x_B = @(x,y) x;
+  y_B = @(x,y) y;
   B = quiver (x, y, x_B(x,y), y_B(x,y), color='r'); hold on
 
   % Define a particle p 
   q_p = 1.6e-19; % charge 
-  m_p = 9.11e-31; % mass
+  m_p = 1.67e-27; % mass
 
-  p_p = [ 0 0 ]; % position 
-  v_p = [ 10 1 ]; % velocity
+  p_p = [ 1 1 ]; % position 
+  v_p = [ 0 0 ]; % velocity
 
   % Get electric and magnetic field vectors at particle's position
   E_p = [ x_E(p_p(1), p_p(2)) y_E(p_p(1), p_p(2)) ];
@@ -31,20 +31,20 @@ function main ()
 
   % Draw initial particle 
   F_p = lorentz_force(q_p, v_p, E_p, B_p);
-  p = quiver (p_p(1), p_p(2), F_p(1)/q_p, F_p(2)/q_p, 'k', "linewidth", 12); hold on
+  p = quiver (p_p(1), p_p(2), F_p(1)/q_p, F_p(2)/q_p, 'k', "linewidth", 2); hold on
   xlim([min max]);
   ylim([min max]);
 
   while true
-    dt = toc/1e5; tic;
+    dt = toc/1e4; tic;
   
     % Update particle position 
     u_p = v_p;
     v_p = u_p + (F_p(1:2)/m_p)*dt;
-    p_p = (v_p + u_p)/2 * dt;
+    p_p = p_p + (v_p + u_p)/2 * dt;
     
     % Get electric and magnetic field vectors at particle's position
-    E_p = [ x_E(p_p(1), p_p(2)) y_E(p_p(1), p_p(2)) ]
+    E_p = [ x_E(p_p(1), p_p(2)) y_E(p_p(1), p_p(2)) ];
     B_p = [ x_B(p_p(1), p_p(2)) y_B(p_p(1), p_p(2)) ];
 
     % Recalculate lorentz_force
@@ -56,7 +56,7 @@ function main ()
     set (p, 'udata', F_p(1)/q_p)
     set (p, 'vdata', F_p(2)/q_p)
 
-    pause (0.001)
+    pause (0.01)
   end
 end
 
